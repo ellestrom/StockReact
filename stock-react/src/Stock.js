944ac62e540b54bbbe5f9293967b0d979db22962
ellestrom <alexander@ellestrom.se>
2023-05-29 12:46:17 +0200

    Update Stock.js

--- stock-react/src/Stock.js
+++ stock-react/src/Stock.js
@@ -2,31 +2,63 @@ import React, { useEffect, useState } from 'react';
 
 const MyComponent = () => {
   const [stockData, setStockData] = useState([]);
+  const [symbol, setSymbol] = useState('');
 
-  useEffect(() => {
+  const handleSymbolChange = (event) => {
+    setSymbol(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchStockData();
+  };
+
+  const fetchStockData = () => {
+    if (!symbol) {
+      setStockData([]);
+      return;
+    }
+
     const apiKey = 'chq7vd9r01qt7cgvtqf0chq7vd9r01qt7cgvtqfg'; // Replace with your Finnhub API key
-    const symbol = 'AAPL'; // Replace with the desired stock symbol
 
     // Fetch stock data from the Finnhub API
     fetch(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${apiKey}`)
       .then(response => response.json())
       .then(data => {
-        const formattedData = [{
-          date: new Date().toLocaleDateString(),
-          open: parseFloat(data.o),
-          high: parseFloat(data.h),
-          low: parseFloat(data.l),
-          close: parseFloat(data.c),
-          volume: parseInt(data.v)
-        }];
-        setStockData(formattedData);
+        if (data.error) {
+          setStockData([]);
+        } else {
+          const formattedData = [{
+            date: new Date().toLocaleDateString(),
+            open: parseFloat(data.o),
+            high: parseFloat(data.h),
+            low: parseFloat(data.l),
+            close: parseFloat(data.c),
+            volume: parseInt(data.v)
+          }];
+          setStockData(formattedData);
+        }
       })
       .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchStockData();
   }, []);
 
   return (
     <div>
       <h1>Stock Data:</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="symbolInput">Enter Stock Symbol:</label>
+        <input
+          type="text"
+          id="symbolInput"
+          value={symbol}
+          onChange={handleSymbolChange}
+        />
+        <button type="submit">Fetch Data</button>
+      </form>
       <table>
         <thead>
           <tr>
@@ -39,16 +71,17 @@ const MyComponent = () => {
           </tr>
         </thead>
         <tbody>
-          {stockData.map(item => (
-            <tr key={item.date}>
-              <td>{item.date}</td>
-              <td>{item.open}</td>
-              <td>{item.high}</td>
-              <td>{item.low}</td>
-              <td>{item.close}</td>
-              <td>{item.volume}</td>
-            </tr>
-          ))}
+        {stockData.map(item => (
+  <tr key={item.date}>
+    <td>{item.date}</td>
+    <td>{item.open.toFixed(2)}</td>
+    <td>{item.high.toFixed(2)}</td>
+    <td>{item.low.toFixed(2)}</td>
+    <td>{item.close.toFixed(2)}</td>
+    <td>{item.volume.toFixed(2)}</td>
+  </tr>
+))}
+
         </tbody>
       </table>
     </div>
